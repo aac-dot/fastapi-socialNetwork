57e1c045d9b5ad1b6da5ebf685773bca75fbab87
kkjj154 <ayrton.andrade1@github.com>
2023-08-06 00:55:58 -0300

    feat: Add the method to create a post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -7,6 +8,12 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get('/posts')
+@app.get("/posts")
 def get_posts():
-    return {'data': 'this is your post'}
+    return {"data": "this is your post"}
+
+@app.post("/create_post")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
+    # return {"title": payload["title"], "content": payload["content"]}
