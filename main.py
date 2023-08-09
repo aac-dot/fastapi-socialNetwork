58b49ab225e93c8b7388e7c8ef9594c73bda8da2
kkjj154 <ayrton.andrade1@github.com>
2023-08-08 22:13:48 -0300

    feat: Add the my_posts list

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 
+MAX_POSTS = 100
 
 app = FastAPI()
 
@@ -12,6 +13,8 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+# my_posts = [{"title": "title of post 1", "content": "content post 1", "id": 1}, {"title": "title of post 2", "content": "content post 1", "id": 1}]
+my_posts = [{"title": f"title of post {i}", "content": f"content post {i}", "id": i} for i in range(1, MAX_POSTS + 1)]
 
 @app.get("/")
 async def root():
@@ -19,13 +22,11 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "this is your post"}
+    return {"data": my_posts}
 
 @app.post("/posts")
 def create_posts(post: Post):
     print(post.rating)
-    # return {"new_post": f"title: {new_post['title']} content: {new_post['content']}"}
-    # return {"title": payload["title"], "content": payload["content"]}
     print(post.model_dump())
     return {"data": post.model_dump()}
     
