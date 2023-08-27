5cd3c90e57e54ff8ab69bdcaa639eac67211495e
kkjj154 <ayrton.andrade1@github.com>
2023-08-27 18:07:43 -0300

    refactor: Create the created_at column for posts

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -8,3 +9,4 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=True, server_default="TRUE")
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
