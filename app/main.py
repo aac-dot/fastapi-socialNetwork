a4dfe3d9ef43b3fcae72a7f244422c7ca41762f5
kkjj154 <ayrton.andrade1@github.com>
2023-09-03 22:19:50 -0300

    refactor: Change the way the update post is done

--- app/main.py
+++ app/main.py
@@ -101,13 +101,21 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     
-    cursor.execute(""" UPDATE pub lic." posts" SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute(""" UPDATE pub lic." posts" SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
     
-    if updated_post is None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    
+    post = post_query.first()
+    
+    if post_query is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} doesn't exist")
     
-    return {"data": updated_post}
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+    
+    db.commit()
+    
+    return {"data": post_query.first()}
