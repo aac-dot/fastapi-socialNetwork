2c27ef69c3298793fa9a239a6244dd2d0a3a7817
kkjj154 <ayrton.andrade1@github.com>
2023-09-10 21:05:19 -0300

    feat: Create Users model

--- app/models.py
+++ app/models.py
@@ -11,3 +11,10 @@ class Post(Base):
     published = Column(Boolean, nullable=True, server_default="TRUE")
     # rating = Column(ARRAY(Integer), nullable=True, server_default="None")
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False, unique=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
